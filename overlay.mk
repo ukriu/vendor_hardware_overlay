1126ce0af1d9963f42167e7c840b324dfe1b7d75
Shirayuki39 <lorddemecrius83@proton.me>
2023-12-26 23:07:12 +0800

    POVA 5 Pro: Added Overlay

--- overlay.mk
+++ overlay.mk
@@ -297,6 +297,8 @@ PRODUCT_PACKAGES += \
 	treble-overlay-tecno-pova4pro-systemui \
         treble-overlay-tecno-pova5 \
         treble-overlay-tecno-pova5-systemui \
+        treble-overlay-tecno-pova5pro \
+        treble-overlay-tecno-pova5pro-systemui \
 	treble-overlay-tecno-spark5 \
 	treble-overlay-tecno-spark6 \
 	treble-overlay-telephony-caf-ims \
